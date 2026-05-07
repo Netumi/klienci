7052530eeaab770031dddffd04efa6e272d6964c
Netumi <fsdbb76@int.pl>
2026-05-07 22:42:51 +0200

    p4

--- api/debug.ts
+++
@@ -1,8 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  return res.status(200).json({ 
-    POSTGRES_URL: process.env.POSTGRES_URL || 'Not set',
-    note: 'DELETE THIS FILE IMMEDIATELY AFTER USE!'
-  });
-}
